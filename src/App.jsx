fcc43ecab001404a71316395531672665a5999da
JappieSoft <j.aeromaint@gmail.com>
2025-05-27 17:10:24 +0200

    Sorted out local folder issue, now some styling for my brain

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,55 @@ function App() {
 
     return (
         <>
-            <h1>Fruitmand bezorgservice</h1>
-            <div>
-                <article>
-                    <h3>Aardbeien</h3>
-                    <button>-</button>
-                    <p>  0  </p>
-                    <button>+</button>
-                </article>
-            </div>
-            <div>
-                <article>
-                    <h3>Peer</h3>
-                    <button>-</button>
-                    <p>  0  </p>
-                    <button>+</button>
-                </article>
-            </div>
-            <div>
-                <article>
-                    <h3>Meloen</h3>
-                    <button>-</button>
-                    <p>  0  </p>
-                    <button>+</button>
-                </article>
-            </div>
-            <div>
-                <article>
-                    <h3>Ananas</h3>
-                    <button>-</button>
-                    <p>  0  </p>
-                    <button>+</button>
-                </article>
-            </div>
+            <header>
+                <h1>Fruitmand bezorgservice</h1>
+            </header>
+            <main>
+                <div>
+                    <article>
+                        <img src="src/assets/strawberry.png" alt="strawberry"/>
+                        <h3>Aardbeien</h3>
+                        <div className="fruitButtons">
+                            <button>-</button>
+                            <p>0</p>
+                            <button>+</button>
+                        </div>
+                    </article>
+                </div>
+                <div>
+                    <article>
+                        <img src="src/assets/pear.png" alt="pear"/>
+                        <h3>Peer</h3>
+                        <div className="fruitButtons">
+                            <button>-</button>
+                            <p>0</p>
+                            <button>+</button>
+                        </div>
+                    </article>
+                </div>
+                <div>
+                    <article>
+                        <img src="src/assets/melon.png" alt="melon"/>
+                        <h3>Meloen</h3>
+                        <div className="fruitButtons">
+                            <button>-</button>
+                            <p>0</p>
+                            <button>+</button>
+                        </div>
+                    </article>
+                </div>
+                <div>
+                    <article>
+                        <img src="src/assets/pineapple.png" alt="pineapple"/>
+                        <h3>Ananas</h3>
+                        <div className="fruitButtons">
+                            <button>-</button>
+                            <p>0</p>
+                            <button>+</button>
+                        </div>
+                    </article>
+                </div>
+            </main>
         </>
     )
 }
