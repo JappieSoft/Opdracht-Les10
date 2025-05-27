abfc86345fa75982996ed23cbd91494d0a39f528
JappieSoft <j.aeromaint@gmail.com>
2025-05-27 16:39:58 +0200

    The Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+            <div>
+                <article>
+                    <h3>Aardbeien</h3>
+                    <button>-</button>
+                    <p>  0  </p>
+                    <button>+</button>
+                </article>
+            </div>
+            <div>
+                <article>
+                    <h3>Peer</h3>
+                    <button>-</button>
+                    <p>  0  </p>
+                    <button>+</button>
+                </article>
+            </div>
+            <div>
+                <article>
+                    <h3>Meloen</h3>
+                    <button>-</button>
+                    <p>  0  </p>
+                    <button>+</button>
+                </article>
+            </div>
+            <div>
+                <article>
+                    <h3>Ananas</h3>
+                    <button>-</button>
+                    <p>  0  </p>
+                    <button>+</button>
+                </article>
+            </div>
+        </>
+    )
 }
 
 export default App
